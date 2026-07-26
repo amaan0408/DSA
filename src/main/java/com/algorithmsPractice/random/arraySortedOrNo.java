660d371b89815086404e678edd0b60a9ce012d18
Amaan <hawaldaramaan@gmail.com>
2026-07-26 21:18:27 +0530

    Is array sorted

--- src/main/java/com/algorithmsPractice/random/arraySortedOrNo.java
+++ src/main/java/com/algorithmsPractice/random/arraySortedOrNo.java
@@ -2,17 +2,17 @@ package com.algorithmsPractice.random;
 
 public class arraySortedOrNo {
 
-        public boolean isSorted(int[] arr){
+        public int isSorted(int[] arr){
             for(int i=1; i<arr.length; i++){
                 if(arr[i-1]>arr[i]){
-                    return false;
+                    return i;
                 }
             }
-            return true;
+            return -1;
         }
     public static void main(String[] args) {
         arraySortedOrNo a = new arraySortedOrNo();
-        int arr[] = {-5,-2,1};
+        int arr[] = {1,3};
         System.out.println(a.isSorted(arr));
         }
     }
