ef2ac93df14b05de87e78c761794a72b6325d2d4
Amaan <hawaldaramaan@gmail.com>
2026-07-26 22:51:43 +0530

    Second Largest Element (with edge case of all similar elements)

--- src/main/java/com/algorithmsPractice/random/secondLargestElement.java
+++ src/main/java/com/algorithmsPractice/random/secondLargestElement.java
@@ -2,6 +2,9 @@ package com.algorithmsPractice.random;
 
 public class secondLargestElement {
     public int secondLargest(int[] arr){
+        if(arr.length==1){
+            return -1;
+        }
         int max = Integer.MIN_VALUE;
         int secMax = max;
         for(int i=0; i<arr.length; i++){
@@ -9,15 +12,15 @@ public class secondLargestElement {
             secMax=max;
             max=arr[i];
             }
-            else if(arr[i]>secMax){
+            else if(arr[i]>secMax && arr[i]!=max){
                     secMax = arr[i];
             }
         }
-        return secMax;
+        return (secMax == Integer.MIN_VALUE) ? -1 : secMax;
     }
     public static void main(String[] args) {
         secondLargestElement nums = new secondLargestElement();
-        int arr [] = {1,2,8,5};
+        int arr [] = {2,2,2,2};
         int result = nums.secondLargest(arr);
         System.out.println(result);
     }
