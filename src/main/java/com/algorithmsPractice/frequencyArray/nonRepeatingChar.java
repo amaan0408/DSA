609c665a6479e0af7c7c1d661b82c1d2a3c8d48e
Amaan <hawaldaramaan@gmail.com>
2026-07-27 20:02:58 +0530

    First Non Repeating Element

---
+++ src/main/java/com/algorithmsPractice/frequencyArray/nonRepeatingChar.java
@@ -0,0 +1,26 @@
+package com.algorithmsPractice.frequencyArray;
+
+import com.algorithmsPractice.BetterWinsOutright.Longest_word;
+
+public class nonRepeatingChar {
+    public int firstNonRepeatingCharacter(String str){
+        //leetcode
+        int fre [] = new int [26];
+        for(int i=0; i<str.length(); i++){
+            fre[str.charAt(i)-'a']++;
+        }
+        for (int i = 0; i < str.length(); i++) {
+            if (fre[str.charAt(i) - 'a'] == 1) {
+                return i; // Found the first unique char in original string order!
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        nonRepeatingChar obj = new nonRepeatingChar();
+        String str = "eetcode";
+        int result = obj.firstNonRepeatingCharacter(str);
+        System.out.println(result);
+
+    }
+}
