0df8640d71806acbb163e4be1fbe71fdff772465
Amaan <hawaldaramaan@gmail.com>
2026-07-27 20:07:58 +0530

    First Non Repeating Element with edge cases

--- src/main/java/com/algorithmsPractice/frequencyArray/nonRepeatingChar.java
+++ src/main/java/com/algorithmsPractice/frequencyArray/nonRepeatingChar.java
@@ -3,19 +3,33 @@ package com.algorithmsPractice.frequencyArray;
 import com.algorithmsPractice.BetterWinsOutright.Longest_word;
 
 public class nonRepeatingChar {
-    public int firstNonRepeatingCharacter(String str){
+    public int firstNonRepeatingCharacter(String str) {
         //leetcode
-        int fre [] = new int [26];
-        for(int i=0; i<str.length(); i++){
-            fre[str.charAt(i)-'a']++;
+        int fre[] = new int[26];
+        for (int i = 0; i < str.length(); i++) {
+            fre[str.charAt(i) - 'a']++;
         }
         for (int i = 0; i < str.length(); i++) {
             if (fre[str.charAt(i) - 'a'] == 1) {
                 return i; // Found the first unique char in original string order!
             }
         }
+        /*
+        int fre[] = new int[256]; // Covers all ASCII characters
+
+    for (int i = 0; i < str.length(); i++) {
+        fre[str.charAt(i)]++; // Direct ASCII conversion without - 'a'
+    }
+
+    for (int i = 0; i < str.length(); i++) {
+        if (fre[str.charAt(i)] == 1) {
+            return i;
+        }
+    }
+         */
         return -1;
     }
+
     public static void main(String[] args) {
         nonRepeatingChar obj = new nonRepeatingChar();
         String str = "eetcode";
