c609fd0c7ae1f04f99fddd982f44e2086c6d55bc
Amaan <hawaldaramaan@gmail.com>
2026-08-31 13:58:15 +0530

    Merge Sort

--- src/main/java/com/algorithmsPractice/random/TCS.java
+++ src/main/java/com/algorithmsPractice/random/TCS.java
@@ -19,9 +19,22 @@ public class TCS {
         }
         return sum;
     }
+
+    public int[] missingAndDuplicate(int arr[]){
+        for(int i=1;i<=arr.length;i++){
+            if(arr[i]==arr[i-1]){
+                return new int []{arr[i], arr[i]+1};
+            }
+        }
+        return new int []{-1,-1};
+    }
+
     public static void main(String[] args) {
         TCS tcs = new TCS();
-        int result = tcs.charges(5);
-        System.out.println(result);
+        int arr [] = {10, 11, 12, 13, 13, 15, 16};
+        int result [] = tcs.missingAndDuplicate(arr);
+        for(int i=0;i<result.length;i++){
+            System.out.print(result[i]+" ");
+        }
     }
 }
---
+++ src/main/java/com/algorithmsPractice/sorting/mergeSort.java
@@ -0,0 +1,56 @@
+package com.algorithmsPractice.sorting;
+
+public class mergeSort {
+
+    public void mergeSorted(int[] arr, int left, int right) {
+
+        if (left >= right) {
+            return;
+        }
+
+        int mid = (left + right) / 2;
+
+        mergeSorted(arr, left, mid);
+        mergeSorted(arr, mid + 1, right);
+
+        int i = left;
+        int j = mid + 1;
+        int index = 0;
+
+        int[] temp = new int[right - left + 1];
+
+        while (i <= mid && j <= right) {
+
+            if (arr[i] <= arr[j]) {
+                temp[index++] = arr[i++];
+            } else {
+                temp[index++] = arr[j++];
+            }
+        }
+
+        while (i <= mid) {
+            temp[index++] = arr[i++];
+        }
+
+        while (j <= right) {
+            temp[index++] = arr[j++];
+        }
+
+        for (int x = 0; x < temp.length; x++) {
+            arr[left + x] = temp[x];
+        }
+    }
+
+    public static void main(String[] args) {
+
+        mergeSort ms = new mergeSort();
+
+        int[] arr = {1, 6, 3, 9};
+
+        ms.mergeSorted(arr, 0, arr.length - 1);
+
+        for (int value : arr) {
+            System.out.print(value + " ");
+        }
+    }
+}
