fba95300405c3dfbcfa6299b07e1e0a316e9020a
Amaan <hawaldaramaan@gmail.com>
2026-09-01 22:32:00 +0530

    Merge Sort again

--- src/main/java/com/algorithmsPractice/sorting/mergeSort.java
+++ src/main/java/com/algorithmsPractice/sorting/mergeSort.java
@@ -3,13 +3,10 @@ package com.algorithmsPractice.sorting;
 public class mergeSort {
 
     public void mergeSorted(int[] arr, int left, int right) {
-
         if (left >= right) {
             return;
         }
-
         int mid = (left + right) / 2;
-
         mergeSorted(arr, left, mid);
         mergeSorted(arr, mid + 1, right);
 
@@ -20,27 +17,62 @@ public class mergeSort {
         int[] temp = new int[right - left + 1];
 
         while (i <= mid && j <= right) {
-
             if (arr[i] <= arr[j]) {
                 temp[index++] = arr[i++];
             } else {
                 temp[index++] = arr[j++];
             }
         }
-
         while (i <= mid) {
             temp[index++] = arr[i++];
         }
-
         while (j <= right) {
             temp[index++] = arr[j++];
         }
-
         for (int x = 0; x < temp.length; x++) {
             arr[left + x] = temp[x];
         }
     }
 
+
+    /*
+            if (left >= right) {
+                return;
+            }
+
+            int mid = (left + right) / 2;
+
+            mergeSorted(arr, left, mid);
+            mergeSorted(arr, mid + 1, right);
+
+            int i = left;
+            int j = mid + 1;
+            int index = 0;
+
+            int[] temp = new int[right - left + 1];
+
+            while (i <= mid && j <= right) {
+
+                if (arr[i] <= arr[j]) {
+                    temp[index++] = arr[i++];
+                } else {
+                    temp[index++] = arr[j++];
+                }
+            }
+
+            while (i <= mid) {
+                temp[index++] = arr[i++];
+            }
+
+            while (j <= right) {
+                temp[index++] = arr[j++];
+            }
+
+            for (int x = 0; x < temp.length; x++) {
+                arr[left + x] = temp[x];
+            }
+        }
+     */
     public static void main(String[] args) {
 
         mergeSort ms = new mergeSort();
@@ -52,5 +84,7 @@ public class mergeSort {
         for (int value : arr) {
             System.out.print(value + " ");
         }
+
+        System.out.println(Integer.compare(1,6));
     }
 }
